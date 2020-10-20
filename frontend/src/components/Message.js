3a1c9e900599d4dae34d74570e4a6dd83a926037
Governnment <governnment@gmail.com>
2020-10-20 12:12:08 +0300

    Message page customization

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Alert } from 'react-bootstrap'
+import errorAnimation from '../video/404-Animation.mp4'
 
-const Message = ({ variant, children }) => {
-  return <Alert variant={variant}>{children}</Alert>
-}
-
-Message.defaultProps = {
-  variant: 'info',
+const Message = ({ children }) => {
+  return (
+    <div>
+      <video className='errorAnimation' autoPlay loop muted>
+        <source src={errorAnimation} type='video/mp4' />
+      </video>
+      <div className='errorText'>
+        <h4>Cap we got a problem here</h4>
+        <h3 className='errorLog'>{children}</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Message
