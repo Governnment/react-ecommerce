ea8ed64f5c4c1b14abde50ec0618e42f6ae28e5c
Governnment <governnment@gmail.com>
2020-10-20 17:38:16 +0300

    FIXED : TypeError: Cannot read property 'length' of null

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -17,5 +17,5 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     },
   })
 
-  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItem))
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,7 +14,7 @@ const reducer = combineReducers({
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItem'))
+  ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
 const initialState = {
