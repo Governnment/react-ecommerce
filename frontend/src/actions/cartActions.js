bd19001dc6d404ee9220d2aa59709a7278bed198
Governnment <governnment@gmail.com>
2020-10-21 10:49:03 +0300

    Remove Items From Cart

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,4 @@
 import axios from 'axios'
-import thunk from 'redux-thunk'
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
@@ -15,6 +14,15 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       countInStock: data.countInStock,
       qty,
     },
+  })
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
   })
 
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
