9740a4e3a6ecfded4d5d4007627bce4eb7ec9547
Governnment <governnment@gmail.com>
2020-10-23 16:57:21 +0300

    Custom Authentication Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,4 +25,24 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser }
+//? @desk     Get user pofile
+//? @rout     GET /api/users/profile
+//? @access   Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
