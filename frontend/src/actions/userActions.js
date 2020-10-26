b002745bb6bf5ff188783d75a0e176117a6073d4
Governnment <governnment@gmail.com>
2020-10-26 15:00:00 +0300

    Show user in navbar & Logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -38,4 +39,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     })
   }
+}
+
+export const logout = () => (dispacth) => {
+  localStorage.removeItem('userInfo')
+  dispacth({ type: USER_LOGOUT })
 }
