5ca03b5f4ccdeba726b520d51a236571fc67eb07
Governnment <governnment@gmail.com>
2020-10-29 21:18:29 +0300

    BUG: Shipping Address dosn't save in local storage after reload

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -32,11 +32,11 @@ export const removeFromCart = (id) => (dispatch, getState) => {
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
 
-export const saveShippingAddress = (data) => (dispatch, getState) => {
+export const saveShippingAddress = (data) => (dispatch) => {
   dispatch({
     type: CART_SAVE_SHIPPING_ADDRESS,
     payload: data,
   })
 
-  localStorage.setItem('shippingAddress', JSON.stringify(getState(data)))
+  localStorage.setItem('shippingAddress', JSON.stringify(data))
 }
