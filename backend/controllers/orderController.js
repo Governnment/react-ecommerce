87af42ae6df25553e7be8bc5764aef51edf3d085
Governnment <governnment@gmail.com>
2020-11-02 18:14:07 +0300

    Get order by ID endpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import Order from '../models/orderModel.js'
 
 //? @desk     Create new order
-//? @rout     POST /api/products
+//? @rout     POST /api/orders
 //? @access   Private
 
 const addOrderItems = asyncHandler(async (req, res) => {
@@ -37,4 +37,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 })
 
-export { addOrderItems }
+//? @desk     Get order by ID
+//? @rout     GET /api/orders/:id
+//? @access   Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  )
+
+  if (order) {
+    res.json(order)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+export { addOrderItems, getOrderById }
